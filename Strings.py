c49334a8e757ad9646561dcbc022deeca0c8109b
Michael <163198007+JustAnotherSithLord@users.noreply.github.com>
2024-03-18 00:05:49 -0500

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -1,1 +1,14 @@
+# Use single quotes
+greeting = 'Hello there!'
+greeting
+'Hello there!'
 
+# Use double quotes
+welcome = "Welcome to Real Python!"
+welcome
+'Welcome to Real Python!'
+
+# Use triple quotes
+message = """Thanks for joining us!"""
+message
+'Thanks for joining us!'
