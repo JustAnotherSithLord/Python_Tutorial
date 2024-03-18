9ec1e04cb28a1fc80f7cb8bc4943ce151572e72e
Michael <163198007+JustAnotherSithLord@users.noreply.github.com>
2024-03-18 00:06:01 -0500

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -12,3 +12,13 @@ welcome
 message = """Thanks for joining us!"""
 message
 'Thanks for joining us!'
+
+# Escape characters
+escaped = 'can\'t'
+escaped
+"can't"
+not_escaped = "can't"
+not_escaped
+"can't"
+
+#
